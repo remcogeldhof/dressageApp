e6fa291bce23ae96209c7d643141fb0e5a1fa2e0
remcogeldhof <geldhofremco@hotmail.com>
2017-11-27 10:41:30 +0100

    Add files via upload

--- home.ts
+++ home.ts
@@ -1,66 +1,113 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
+
+//import * as $ from 'jquery'
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController) {
 
-  }
+	}
 
-
-
-  //bron: https://www.w3schools.com/howto/howto_js_animate.asp
-
-
-
-  play() {
-    this.myMove();
-    //this.myMove2();
-
-  }
-
-  myMove() {
-
-  var width = screen.width;
-  var height = screen.height;
-  var speed = 0;
-
-  var elem = document.getElementById("myAnimation");
-  var pos = 0;
-  var id = setInterval(frame, speed);
-  function frame() {
-    if (pos == height-200) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.top = pos + 'px';
-     // elem.style.left = pos + 'px';
+    load() {
+      this.navCtrl.push(ListPage);
     }
-    
-  }
-  }
 
-  myMove2() {
+	//bron: https://www.w3schools.com/howto/howto_js_animate.asp
 
-    var width = screen.width;
-    var height = screen.height;
-    var speed = 0;
+  
 
-    var elem = document.getElementById("myAnimation");
-    var pos = 0;
-    var id = setInterval(frame, speed);
-    function frame() {
-      if (pos == width - 220) {
-        clearInterval(id);
-      } else {
-        pos++;
-        elem.style.left = pos + 'px';
-      }
+	play() {
+		this.verticaal();
+		
+		//this.horizontaal();
+	}
 
-    }
-  }
+
+  myFunction() {
+    document.getElementById("myAnimation").style.animation = "mymove1 8s";
+   // document.getElementById("myAnimation").style.animation = "mymove2 3s";
+}
+
+
+	verticaal() {	
+		var speed = 0;
+
+		var elem = document.getElementById("myAnimation");
+		var pos = 0; 
+		var id = setInterval(frame, speed);
+		function frame() {
+			if (pos == 429) {
+				clearInterval(id);
+			} else {
+				pos++;
+				elem.style.top = pos + 'px';
+				elem.style.left = 0 + 'px';
+			}
+
+		}
+	}
+
+	horizontaal() {
+		var speed = 0;
+
+
+		var elem = document.getElementById("myAnimation");
+		var pos = 0;
+		var id = setInterval(frame, speed);
+		function frame() {
+			if (pos == 129) {
+				clearInterval(id);
+			} else {
+				pos++;
+				elem.style.left = pos + 'px';
+			}
+
+		}
+	}
+
+
+	diagonaal() {
+
+
+
+		var speed = 0; //lengte animatie
+
+		var elem = document.getElementById("myAnimation");
+		var pos = 0;
+		var id = setInterval(frame, speed);
+		function frame() {
+			if (pos == 100 ) {
+				clearInterval(id);
+			} else {
+				pos++;
+				elem.style.top = pos + 'px';
+				elem.style.left = pos + 'px';
+
+			}
+
+		}
+	}
+
+	test() {
+		const maxXPosition = 100;
+		let rect = document.getElementById('myAnimation');
+		let speedX = 2;
+		let positionX = 0;
+		function step() {
+			positionX = positionX + speedX;
+			if (positionX > maxXPosition || positionX < 0) {
+				speedX = speedX * (-1);
+			}
+			rect.style.left = positionX + 'px';
+			window.requestAnimationFrame(step);
+		}
+		window.requestAnimationFrame(step);
+	}  
+
 }
